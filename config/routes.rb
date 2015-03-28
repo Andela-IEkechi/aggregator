d2da04238fac51787918be6bed91b72d15439444
Ekechi Ikenna <luccy559slim@gmail.com>
2015-03-28 13:21:27 +0100

    began styling of landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'home#show'
+  get '/dashboard', to: 'dashboard#show'
 
   devise_for :users
-  resources :dashboard
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
